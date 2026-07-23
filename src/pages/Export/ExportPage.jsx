b76457f2ae1f1281c7add975d16f59e43c2502b0
Biruk Bizuayehu <birukbizuayehu1167@gmail.com>
2026-07-23 17:04:27 +0300

    ui update

--- src/pages/Export/ExportPage.jsx
+++ src/pages/Export/ExportPage.jsx
@@ -366,7 +366,7 @@ export const ExportPage = () => {
   const handlePageChange = (pageNum) => setCurrentPage(pageNum);
 
   return (
-    <div className="flex-1 space-y-6 p-4 md:p-8 max-w-7xl mx-auto w-full">
+    <div className="flex-1  p-4 md:p-8 max-w-7xl mx-auto w-full">
       {isVisible && (
         <button
           onClick={scrollToTop}

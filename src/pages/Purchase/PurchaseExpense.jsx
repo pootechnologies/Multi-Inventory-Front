280870710426ee062c09c7386b27a7eab0d8d2ad
Biruk Bizuayehu <birukbizuayehu1167@gmail.com>
2026-07-23 17:05:48 +0300

    ui update

--- src/pages/Purchase/PurchaseExpense.jsx
+++ src/pages/Purchase/PurchaseExpense.jsx
@@ -199,7 +199,7 @@ const PurchaseExpense = () => {
   const totalPages = Math.ceil(filteredData.length / itemsPerPage);
 
   return (
-    <div className="flex-1 space-y-6 p-4 md:p-8 max-w-7xl mx-auto w-full">
+    <div className="flex-1  p-4 md:p-8 max-w-7xl mx-auto w-full">
       {isVisible && (
         <button
           onClick={scrollToTop}

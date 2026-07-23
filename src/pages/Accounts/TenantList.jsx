69db114b35e9ca556a7b014e5f63dd155dcb8a54
Biruk Bizuayehu <birukbizuayehu1167@gmail.com>
2026-07-23 17:04:00 +0300

    ui update

--- src/pages/Accounts/TenantList.jsx
+++ src/pages/Accounts/TenantList.jsx
@@ -415,7 +415,7 @@ const TenantList = () => {
   };
 
   return (
-    <div className="flex-1 space-y-6 p-4 md:p-8 max-w-7xl mx-auto w-full">
+    <div className="flex-1  p-4 md:p-8 max-w-7xl mx-auto w-full">
       {isVisible && (
         <button
           onClick={scrollToTop}

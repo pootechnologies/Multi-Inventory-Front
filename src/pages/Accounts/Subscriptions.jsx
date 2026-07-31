bf66492ba748546e9c2f8ac98a0ae80d14aded80
Biruk Bizuayehu <birukbizuayehu1167@gmail.com>
2026-07-31 11:59:02 +0300

    show retray to payment to pay again

--- src/pages/Accounts/Subscriptions.jsx
+++ src/pages/Accounts/Subscriptions.jsx
@@ -1302,7 +1302,7 @@ export default function Subscriptions() {
                   } catch (err) {
                     console.error("Payment initiation failed:", err);
                   } finally {
-                    setIsSubProcessing(false);
+      F              setIsSubProcessing(false);
                   }
                 }}
               >

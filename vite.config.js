12e3956544a0026d3bc8e3ca4bd852a9e16f741c
Biruk Bizuayehu <birukbizuayehu1167@gmail.com>
2026-07-31 17:40:56 +0300

    remove pwa

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      manifest: {
-        name: "Multi-Inventory",
-        short_name: "Inventory",
-        description: "Multi-Inventory Management System",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
   ],
   resolve: {
     alias: {

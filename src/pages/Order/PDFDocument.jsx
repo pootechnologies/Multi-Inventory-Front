859466cc05992f6b73aae0e1526d51c057498fb4
Biruk Bizuayehu <birukbizuayehu1167@gmail.com>
2026-08-05 23:21:18 +0300

    image url fix

--- src/pages/Order/PDFDocument.jsx
+++ src/pages/Order/PDFDocument.jsx
@@ -391,7 +391,7 @@ const MyDoc = ({
               >
                 <Image
                   style={{ width: 80, height: 60 }}
-                  src={`${IMAGE_BASE_URL}${companyData?.logo}`}
+                  src={`${companyData?.logo}`}
                 />
                 <View
                   style={{

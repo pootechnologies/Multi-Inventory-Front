1d7c70917c9b2c3195601275f25aca9402004f08
Biruk <birukbizuayehu1167@gmail.com>
2026-08-20 19:09:43 +0500

    change paggination array

--- src/pages/Order/OrderDetailPage.jsx
+++ src/pages/Order/OrderDetailPage.jsx
@@ -393,7 +393,7 @@ const OrderDetailPage = () => {
                 {t("previous") || "Previous"}
               </Button>
               <div className="flex items-center gap-1 mx-2">
-                {Array.from({ length: Math.min(5, totalPages) }, (_, i) => {
+                {Array.from({ length: Math.min(3, totalPages) }, (_, i) => {
                   let pageNum = i + 1;
                   if (totalPages > 5) {
                     if (modalCurrentPage > 3) {

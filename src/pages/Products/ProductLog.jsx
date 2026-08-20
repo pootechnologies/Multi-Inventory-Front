0790bd9302db40be385c002ad30083616d6597bd
Biruk <birukbizuayehu1167@gmail.com>
2026-08-20 19:10:09 +0500

    change paggination array

--- src/pages/Products/ProductLog.jsx
+++ src/pages/Products/ProductLog.jsx
@@ -348,7 +348,7 @@ const ProductLog = () => {
                   {t("previous")}
                 </Button>
                 <div className="flex items-center gap-1 mx-2">
-                  {Array.from({ length: Math.min(5, totalPages) }, (_, i) => {
+                  {Array.from({ length: Math.min(3, totalPages) }, (_, i) => {
                     let pageNum = i + 1;
                     if (totalPages > 5) {
                       if (currentPage > 3) {
